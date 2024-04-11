304d1fc8006d88c92a2d9986d3d8e1367c99cef0
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-04-11 19:58:52 -0300

    refactoring the code

--- Exemple.bat
+++ Exemple.bat
@@ -1,33 +1,33 @@
 @echo OFF
 
 :: Prompt the user to enter the name of the text file to be created
-set /p nome_arquivo=Enter the name of the text file to be created (include the .txt extension): 
+set /p fileName=Enter the name of the text file to be created (include the .txt extension): 
 
 echo Collecting information...
 
 :: Get the student code entered by the user
-set codigo_estudante=%username%
+set UserName=%username%
 
 :: Get the computer name
-:: Use the 'hostname' command to obtain the computer name and store it in the 'nome_computador' variable
-for /f "tokens=1 " %%A in ('hostname') do set nome_computador=%%A
+:: Use the 'hostname' command to obtain the computer name and store it in the 'computer_name' variable
+for /f "tokens=1 " %%A in ('hostname') do set computer_name=%%A
 
 :: Get the computer's IP address
-:: Use the 'ipconfig' command to obtain the computer's IPv4 address and store it in the 'ip_computador' variable
-for /f "tokens=2 delims=:" %%B in ('ipconfig ^| find "IPv4"') do set ip_computador=%%B
+:: Use the 'ipconfig' command to obtain the computer's IPv4 address and store it in the 'computer_ip' variable
+for /f "tokens=2 delims=:" %%B in ('ipconfig ^| find "IPv4"') do set computer_ip=%%B
 
 :: Get the current time
-:: Use the 'time /t' command to get the current time and store it in the 'hora_atual' variable
-for /f "tokens=1-3 delims=: " %%C in ('time /t') do set hora_atual=%%C:%%D %%E
+:: Use the 'time /t' command to get the current time and store it in the 'date' variable
+for /f "tokens=1-3 delims=: " %%C in ('time /t') do set date=%%C:%%D %%E
 
 :: Write the collected information to the text file specified by the user
-echo Student code: %codigo_estudante% >> %nome_arquivo%
-echo Computer name: %nome_computador% >> %nome_arquivo%
-echo Computer IP: %ip_computador% >> %nome_arquivo%
-echo Current time: %hora_atual% >> %nome_arquivo%
+echo Username: %UserName% >> %fileName%
+echo Computer name: %computer_name% >> %fileName%
+echo Computer IP: %computer_ip% >> %fileName%
+echo Current time: %date% >> %fileName%
 
 :: Inform the user that the information has been saved to the file
-echo The information has been saved to the file %nome_arquivo%.
+echo The information has been saved to the file %fileName%.
 
 :: Wait for the user to press a key before ending the script
 pause
